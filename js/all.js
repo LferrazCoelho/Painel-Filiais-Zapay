484f79762e43bbe85a60105cab0c8185a80817fd
LferrazCoelho <lferraz.dev@gmail.com>
2019-11-26 15:14:36 -0300

    Add Projeto

---
+++ js/all.js
@@ -0,0 +1,15 @@
+function down(){
+    alert("Donwload realizado com sucesso");
+}
+
+function email(){
+    alert("Email enviado com sucesso");
+}
+
+function email(){
+    alert("Email enviado com sucesso");
+}
+
+function helpMkt(){
+    alert("Em breve entramos em contato");
+}
---
+++ js/dom-login.js
@@ -0,0 +1,1 @@
+console.log("arquivo dom-login.js carregado")
